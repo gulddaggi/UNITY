9d5649483f60cd8980953f1d47cdd8ec60fbc89f
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2021-04-04 22:35:11 +0900

    Add files via upload

---
+++ 03CodeStartAndReading/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03CodeStartAndReading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //F10으로 디버깅
+            Console.WriteLine("하하");
+            Console.WriteLine("호호");
+
+        }
+    }
+}
