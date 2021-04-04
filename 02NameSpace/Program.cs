c7563e15be826cd11843834dc81bd4c094165148
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2021-04-04 22:34:56 +0900

    Add files via upload

---
+++ 02NameSpace/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* namespace는 개념의 표현이라기 보다는 개념의 분류
+ */
+
+
+namespace APRO
+{
+    class /* APRO. */Potion
+    {
+
+    }
+
+
+}
+
+namespace BPRO
+{
+    class Potion
+    {
+
+    }
+
+}
