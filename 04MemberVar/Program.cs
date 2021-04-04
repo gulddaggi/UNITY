571d0689f5d49df1ee3a8f272fca92dddc503b2e
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2021-04-04 22:35:51 +0900

    Add files via upload

---
+++ 04MemberVar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Player
+{
+    /*
+    세부속성이면서 명사이면서 값을 가지는 것이 멤버변수
+    멤버변수의 선언은 int(자료형) 이름(식별자)
+    초기값(리터럴값)을 넣어줄 수 있다.
+    선언과 동시에 값을 넣어주는 것을 리터럴 초기화라고 한다.
+     */
+    int Gold = 1000;
+    int Exp;
+    int Att;
+    int Def;
+    int Hp;
+}
