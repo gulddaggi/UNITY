7a347302a63c46ec2ad41a15f2fe7693f2d58dfe
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2021-04-05 02:04:15 +0900

    Add files via upload

---
+++ 05Memberfunc/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Player
+{
+
+    int ATT;
+    int HP;
+
+    //행동 등을 표현하는 것이 맴버함수이다.
+
+    /*리턴값*/
+    void /*이름*/Move /*인자값*/()
+    {//<- 함수 시작
+
+    }//<- 함수 끝
+
+    void Fight()
+    {
+
+
+    }
+
+    void Talk()
+    {
+
+    }
+
+    void SkillUse()
+    {
+
+    }
+
+}
+
+/*RPG를 만든다. -> 프로젝트 만든다.
+  주인공이 있다. -> 클래스
+  주인공이 공격력이 있다. -> 맴버변수
+  주인공이 공격력으로 때린다. -> 맴버함수
+*/
