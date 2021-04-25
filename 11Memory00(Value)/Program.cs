eb905c5e607c22e39b5006e16994a78054619af7
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2021-04-26 02:01:27 +0900

    Add files via upload

---
+++ 11Memory00(Value)/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Player
+{
+    int AT;
+    int HP;
+
+
+    public int Test(int _Dmg) // value의 값만 받은 것
+    {
+        _Dmg = 1000;
+        return _Dmg;
+    }
+}
+
+
+
+namespace _11Memory00_Value_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //함수의 인자값
+            int Value = 0;
+
+
+            Player NewPlayer = new Player();
+            Value = NewPlayer.Test(Value);
+            //값형의 처리
+            //값과 위치에 대한 이해가 필요.
+            Console.WriteLine(Value);
+
+
+        }
+    }
+}
