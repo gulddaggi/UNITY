c83dc19de669d5de090a817672eba61c2348ac45
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2021-05-10 18:33:58 +0900

    Add files via upload

---
+++ 27ClassFile/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+//컨트롤 k + f : 자동코드정렬
+class Program
+{
+    static void Main(string[] args)
+    {
+        Player NewPlayer = new Player();
+
+    }
+}
